e1597cf865f4bd52d40f7f90f93d90fa8582dfa1
Kyeorae Yeom <duarufp06@gmail.com>
2025-07-14 11:49:53 +0900

    Geometry

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,7 @@ scene.background = new THREE.Color(0xffe287)
 // 2. Camera: Scene을 바라볼 시점을 결정
 const camera = new THREE.PerspectiveCamera(50, $result.clientWidth / $result.clientHeight, 0.1, 1000);
 camera.position.set(2, 2, 2);
+// camera.position.set(0, 0, 5);
 camera.lookAt(0, 0, 0)
 
 // 3. Renderer: Scene+Camera, 화면을 그려주는 역할
@@ -23,13 +24,48 @@ const light = new THREE.DirectionalLight(0xffffff)
 light.position.set(2, 4, 3)
 scene.add(light)
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshStandardMaterial({ color: 0x2E6FF2 })
-const box = new THREE.Mesh(geometry, material)
-scene.add(box);
+// const box = new THREE.Mesh(geometry, material)  // Mesh: 3차원, geometry: 형태, material: 재질
+// scene.add(box);
+
+// 육면체
+const geo1 = new THREE.BoxGeometry(1, 1, 1);
+const obj1 = new THREE.Mesh(geo1, material);
+// scene.add(obj1);
+
+// 원뿔
+const geo2 = new THREE.ConeGeometry(0.5, 1, 32);
+const obj2 = new THREE.Mesh(geo2, material);
+// scene.add(obj2);
+
+// 원기둥
+const geo3 = new THREE.CylinderGeometry(0.5, 0.8, 1);
+const obj3 = new THREE.Mesh(geo3, material);
+// scene.add(obj3);
+
+// 구
+const geo4 = new THREE.SphereGeometry(1);
+const obj4 = new THREE.Mesh(geo4, material);
+// scene.add(obj4);
+
+// 평면
+const geo5 = new THREE.PlaneGeometry(1, 2);
+const obj5 = new THREE.Mesh(geo5, material);
+// scene.add(obj5);
+
+// 원
+const geo6 = new THREE.CircleGeometry(1, 32);
+const obj6 = new THREE.Mesh(geo6, material);
+// scene.add(obj6)
+
+// 원
+const geo7 = new THREE.TorusGeometry(1, 0.5);
+const obj7 = new THREE.Mesh(geo7, material);
+scene.add(obj7)
 
 function animate() {
-  box.rotation.y += 0.01;
+  // box.rotation.y += 0.01;
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
 }
