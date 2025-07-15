dbc9dbdbc877fd3b64799e0bc1f1b6e34687acfc
Kyeorae Yeom <duarufp06@gmail.com>
2025-07-15 16:47:11 +0900

    Mesh

--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,7 @@ scene.background = new THREE.Color(0xffe287)
 
 // 2. Camera: Scene을 바라볼 시점을 결정
 const camera = new THREE.PerspectiveCamera(50, $result.clientWidth / $result.clientHeight, 0.1, 1000);
-camera.position.set(2, 2, 2);
+camera.position.set(5, 5, 5);
 // camera.position.set(0, 0, 5);
 camera.lookAt(0, 0, 0)
 
@@ -24,79 +24,28 @@ const light = new THREE.DirectionalLight(0xffffff)
 light.position.set(2, 4, 3)
 scene.add(light)
 
-const geometry = new THREE.BoxGeometry(1);
-// Material
-const basic = new THREE.MeshBasicMaterial({
+const geometry = new THREE.DodecahedronGeometry(1);
+const material = new THREE.MeshStandardMaterial({
   color: 0xffaaaa,
-  // wireframe: true,
-  transparent: true,
-  opacity: 0.5
 });
-
-const standard = new THREE.MeshStandardMaterial({
-  color: 0xffaaaa,
-  roughness: 0.2,
-  metalness: 0.8,
-  // map: ,
-  side: THREE.FrontSide
-});
-
-const physical = new THREE.MeshPhysicalMaterial({
-  color: 0xffaaaa,
-  clearcoat: 0.8,
-  clearcoatRoughness: 0.2
-});
-
-const phong = new THREE.MeshPhongMaterial({
-  color: 0xffaaaa,
-  shininess: 100,
-  specular: 0x2e6ff2
-});
-
-const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), standard);
-const mesh = new THREE.Mesh(geometry, phong);
+const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
-// scene.add(plane);
 
-// const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshStandardMaterial({ color: 0x2E6FF2 })
-// const box = new THREE.Mesh(geometry, material)  // Mesh: 3차원, geometry: 형태, material: 재질
-// scene.add(box);
+// axesHelper
+const axesHelper = new THREE.AxesHelper(10);
+scene.add(axesHelper)
 
-// 육면체
-const geo1 = new THREE.BoxGeometry(1, 1, 1);
-const obj1 = new THREE.Mesh(geo1, material);
-// scene.add(obj1);
+// 1. 위치
+// Mesh.position.x = 2;
+// mesh.position.y = 1;
+mesh.position.set(0, 2, 1);
 
-// 원뿔
-const geo2 = new THREE.ConeGeometry(0.5, 1, 32);
-const obj2 = new THREE.Mesh(geo2, material);
-// scene.add(obj2);
+// 2. 회전
+mesh.rotation.y = THREE.MathUtils.degToRad(30);
 
-// 원기둥
-const geo3 = new THREE.CylinderGeometry(0.5, 0.8, 1);
-const obj3 = new THREE.Mesh(geo3, material);
-// scene.add(obj3);
-
-// 구
-const geo4 = new THREE.SphereGeometry(1);
-const obj4 = new THREE.Mesh(geo4, material);
-// scene.add(obj4);
-
-// 평면
-const geo5 = new THREE.PlaneGeometry(1, 2);
-const obj5 = new THREE.Mesh(geo5, material);
-// scene.add(obj5);
-
-// 원
-const geo6 = new THREE.CircleGeometry(1, 32);
-const obj6 = new THREE.Mesh(geo6, material);
-// scene.add(obj6)
-
-// 원
-const geo7 = new THREE.TorusGeometry(1, 0.5);
-const obj7 = new THREE.Mesh(geo7, material);
-// scene.add(obj7)
+// 3. 크기
+mesh.scale.x = 1.2
+mesh.scale.z = 0.8
 
 function animate() {
   // box.rotation.y += 0.01;
