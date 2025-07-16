ccb2dd6640f74cb3d82660d20da1d64cf99862b4
Kyeorae Yeom <duarufp06@gmail.com>
2025-07-16 23:49:16 +0900

    practice: fruit & tree

--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,7 @@ scene.background = new THREE.Color(0xffe287)
 
 // 2. Camera: Scene을 바라볼 시점을 결정
 const camera = new THREE.PerspectiveCamera(50, $result.clientWidth / $result.clientHeight, 0.1, 1000);
-camera.position.set(5, 5, 5);
+camera.position.set(10, 10, 10);
 // camera.position.set(0, 0, 5);
 camera.lookAt(0, 0, 0)
 
@@ -24,12 +24,86 @@ const light = new THREE.DirectionalLight(0xffffff)
 light.position.set(2, 4, 3)
 scene.add(light)
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({
-  color: 0x2E6FF2,
+// 한라봉
+// const bodyMaterial = new THREE.MeshStandardMaterial({ color: 0xff7f00 });
+// const bottomGeometry = new THREE.DodecahedronGeometry(2, 1);
+// const bottom = new THREE.Mesh(bottomGeometry, bodyMaterial);
+// scene.add(bottom);
+
+// const topGeometry = new THREE.TetrahedronGeometry(0.8, 3);
+// const top = new THREE.Mesh(topGeometry, bodyMaterial);
+// scene.add(top);
+// top.position.y = 1.7;
+
+// const leafMaterial = new THREE.MeshStandardMaterial({ color: 0x008000, side: THREE.DoubleSide });
+// const stemGeometry = new THREE.CylinderGeometry(0.1, 0.1, 0.4);
+// const stem = new THREE.Mesh(stemGeometry, leafMaterial);
+// scene.add(stem);
+// stem.position.y = 2.5;
+
+// const leafGeometry = new THREE.SphereGeometry(0.5, 32, 16, 0, Math.PI / 3);
+// const leaf = new THREE.Mesh(leafGeometry, leafMaterial);
+// scene.add(leaf);
+// leaf.position.set(-0.5, 2.4, -0.1);
+// leaf.rotation.z = Math.PI / -2;
+
+// 나무
+const trunkMaterial = new THREE.MeshStandardMaterial({
+  color: 0xa38049
 });
-const box = new THREE.Mesh(geometry, material);
-scene.add(box);
+const trunkGeometry = new THREE.CylinderGeometry(0.8, 1, 1.5);
+
+const trunk1 = new THREE.Mesh(trunkGeometry, trunkMaterial);
+scene.add(trunk1);
+
+const trunk2 = new THREE.Mesh(trunkGeometry, trunkMaterial);
+trunk2.position.set(0.1, 1.3, 0);
+trunk2.scale.set(0.9, 0.9, 0.9);
+trunk2.rotation.z = THREE.MathUtils.degToRad(-5);
+scene.add(trunk2);
+
+const trunk3 = new THREE.Mesh(trunkGeometry, trunkMaterial);
+trunk3.position.set(0.2, 2.5, 0);
+trunk3.scale.set(0.8, 0.8, 0.8);
+trunk3.rotation.z = THREE.MathUtils.degToRad(-5);
+scene.add(trunk3);
+
+const trunk4 = new THREE.Mesh(trunkGeometry, trunkMaterial);
+trunk4.position.set(0.3, 3.5, 0);
+trunk4.scale.set(0.7, 0.7, 0.7);
+trunk4.rotation.z = THREE.MathUtils.degToRad(-8);
+scene.add(trunk4);
+
+const leafMaterial = new THREE.MeshStandardMaterial({
+  color: 0x84ad88,
+  side: THREE.DoubleSide
+});
+const leafGeometry = new THREE.SphereGeometry(2, 32, 16, Math.PI / 3, Math.PI / 3);
+const leaf1 = new THREE.Mesh(leafGeometry, leafMaterial);
+leaf1.rotation.x = Math.PI / -2;
+leaf1.position.set(0, 3.2, 2);
+scene.add(leaf1);
+
+const leaf2 = new THREE.Mesh(leafGeometry, leafMaterial);
+leaf2.rotation.x = Math.PI / -2;
+leaf2.rotation.z = Math.PI / 2;
+leaf2.position.set(2, 3.2, 0);
+scene.add(leaf2);
+
+const leaf3 = new THREE.Mesh(leafGeometry, leafMaterial);
+leaf3.rotation.x = Math.PI / -2;
+leaf3.rotation.z = Math.PI;
+leaf3.position.set(0, 3.2, -2);
+scene.add(leaf3);
+
+const leaf4 = new THREE.Mesh(leafGeometry, leafMaterial);
+leaf4.rotation.x = Math.PI / -2;
+leaf4.rotation.z = Math.PI / -2;
+leaf4.position.set(-2, 3.2, 0);
+scene.add(leaf4);
+
+const axes = new THREE.AxesHelper(10);
+scene.add(axes);
 
 // OrbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -38,14 +112,14 @@ const controls = new OrbitControls(camera, renderer.domElement);
 // controls.enableRotate = false;
 // controls.enablePan = false;
 
-controls.minDistance = 2
-controls.maxDistance = 10;
+// controls.minDistance = 2
+// controls.maxDistance = 10;
 // controls.maxPolarAngle = Math.PI / 3;
 
-controls.autoRotate = true;
-controls.autoRotateSpeed = -10;
+// controls.autoRotate = true;
+// controls.autoRotateSpeed = -10;
 
-controls.enableDamping = true;
+// controls.enableDamping = true;
 
 function animate() {
   // box.rotation.y += 0.01;
