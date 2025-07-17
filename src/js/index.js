a3449f412bb13df9f1219a8e52d4d4c91ae17a83
Kyeorae Yeom <duarufp06@gmail.com>
2025-07-17 22:50:26 +0900

    Group

--- src/js/index.js
+++ src/js/index.js
@@ -48,32 +48,38 @@ scene.add(light)
 // leaf.rotation.z = Math.PI / -2;
 
 // 나무
+const tree = new THREE.Group();
+const trunk = new THREE.Group();
+
 const trunkMaterial = new THREE.MeshStandardMaterial({
   color: 0xa38049
 });
 const trunkGeometry = new THREE.CylinderGeometry(0.8, 1, 1.5);
 
 const trunk1 = new THREE.Mesh(trunkGeometry, trunkMaterial);
-scene.add(trunk1);
+trunk.add(trunk1);
 
 const trunk2 = new THREE.Mesh(trunkGeometry, trunkMaterial);
 trunk2.position.set(0.1, 1.3, 0);
 trunk2.scale.set(0.9, 0.9, 0.9);
 trunk2.rotation.z = THREE.MathUtils.degToRad(-5);
-scene.add(trunk2);
+trunk.add(trunk2);
 
 const trunk3 = new THREE.Mesh(trunkGeometry, trunkMaterial);
 trunk3.position.set(0.2, 2.5, 0);
 trunk3.scale.set(0.8, 0.8, 0.8);
 trunk3.rotation.z = THREE.MathUtils.degToRad(-5);
-scene.add(trunk3);
+trunk.add(trunk3);
 
 const trunk4 = new THREE.Mesh(trunkGeometry, trunkMaterial);
 trunk4.position.set(0.3, 3.5, 0);
 trunk4.scale.set(0.7, 0.7, 0.7);
 trunk4.rotation.z = THREE.MathUtils.degToRad(-8);
-scene.add(trunk4);
+trunk.add(trunk4);
 
+tree.add(trunk);
+
+const leaf = new THREE.Group();
 const leafMaterial = new THREE.MeshStandardMaterial({
   color: 0x84ad88,
   side: THREE.DoubleSide
@@ -82,25 +88,33 @@ const leafGeometry = new THREE.SphereGeometry(2, 32, 16, Math.PI / 3, Math.PI / 
 const leaf1 = new THREE.Mesh(leafGeometry, leafMaterial);
 leaf1.rotation.x = Math.PI / -2;
 leaf1.position.set(0, 3.2, 2);
-scene.add(leaf1);
+leaf.add(leaf1);
 
 const leaf2 = new THREE.Mesh(leafGeometry, leafMaterial);
 leaf2.rotation.x = Math.PI / -2;
 leaf2.rotation.z = Math.PI / 2;
 leaf2.position.set(2, 3.2, 0);
-scene.add(leaf2);
+leaf.add(leaf2);
 
 const leaf3 = new THREE.Mesh(leafGeometry, leafMaterial);
 leaf3.rotation.x = Math.PI / -2;
 leaf3.rotation.z = Math.PI;
 leaf3.position.set(0, 3.2, -2);
-scene.add(leaf3);
+leaf.add(leaf3);
 
 const leaf4 = new THREE.Mesh(leafGeometry, leafMaterial);
 leaf4.rotation.x = Math.PI / -2;
 leaf4.rotation.z = Math.PI / -2;
 leaf4.position.set(-2, 3.2, 0);
-scene.add(leaf4);
+leaf.add(leaf4);
+
+tree.add(leaf);
+
+tree.position.x = 2;
+scene.add(tree);
+
+leaf.position.x = -0.4;
+leaf.rotation.z = THREE.MathUtils.degToRad(-10);
 
 const axes = new THREE.AxesHelper(10);
 scene.add(axes);
